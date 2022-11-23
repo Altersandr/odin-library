30f0cbd8d001a3000d06ce311b0e371b359aa75d
Oleks <archer.opendub@gmail.com>
2022-11-23 13:04:08 +0100

    boilerplate html design and some css styling as well as some js for testing

---
+++ script.js
@@ -0,0 +1,26 @@
+let myLibrary = [{
+    name: "At the Mountains of Madness",
+    author: "H.P. Lovecrafr"
+},{
+    name: "Sherlock Holmes",
+    author: "Arthur Conan Doyle"
+},{
+    name: "Lords of the Rings",
+    author: "J.R.R. Tolkien"
+}
+
+
+
+
+
+];
+
+function Book(){
+
+}
+
+function addBookToLibrary(){
+
+}
+
+console.log(myLibrary[2])
