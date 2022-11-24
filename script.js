32f28931053d729f46e1e22b6145c1676f4b3462
Oleks <archer.opendub@gmail.com>
2022-11-24 23:02:45 +0100

    the thing just wont work the way i want it to

--- script.js
+++ script.js
@@ -1,26 +1,97 @@
-let myLibrary = [{
-    name: "At the Mountains of Madness",
-    author: "H.P. Lovecrafr"
-},{
-    name: "Sherlock Holmes",
-    author: "Arthur Conan Doyle"
-},{
-    name: "Lords of the Rings",
-    author: "J.R.R. Tolkien"
-}
-
-
-
-
-
+let myLibrary = [
+//     {
+//     name: "At the Mountains of Madness",
+//     author: "H.P. Lovecraft"
+// },{
+//     name: "Sherlock Holmes",
+//     author: "Arthur Conan Doyle"
+// },{
+//     name: "Lords of the Rings",
+//     author: "J.R.R. Tolkien"
+// }
 ];
 
-function Book(){
 
+
+function Book(name, author, pages, cover){
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.cover = cover;
+    return this
 }
 
-function addBookToLibrary(){
+const addBook = document.getElementById('add');
+addBook.addEventListener('click', addBookToLibrary);
 
-}
+function addBookToLibrary(e){
+        e.preventDefault();
+        
+        const inputName = document.getElementById('bookname').value;
+        const inputAuthor = document.getElementById('authorname').value;
+        const inputPages = document.getElementById('pages').value;
+        const inputCover = document.getElementById('cover').value;
+        const newBook = new Book(inputName, inputAuthor, inputPages, inputCover);
+        myLibrary.push(newBook)
+        
+        const container = document.getElementById('main-container');
+        container.innerHTML = "";
+        
+        myLibrary.forEach(book=>{
+        let bookentry = document.createElement('div');
+        bookentry.classList = "book";
+        let author = document.createElement('div');
+        author.classList = "author";
+        author.textContent = book.author
+        let name = document.createElement('div');
+        name.classList = "name";
+        name.textContent = book.name;
+        let pages = document.createElement("div");
+        pages.classList = "pages";
+        pages.textContent = book.pages + " pages";
+        // console.log(bookentry);
+        bookentry.style.backgroundImage = `url(${inputCover})`;
+        
+        bookentry.dataset.id = myLibrary.indexOf(book);
+        bookentry.appendChild(author);
+        bookentry.appendChild(name);
+        bookentry.appendChild(pages);
+        container.appendChild(bookentry);
 
-console.log(myLibrary[2])
+        let controls = document.createElement('div');
+        controls.id = "controls";
+        let readBtn = document.createElement('button');
+        readBtn.textContent = 'Read';
+        let deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.className = 'deleteBtn';
+        deleteBtn.dataset.id = myLibrary.indexOf(book);
+        // console.log(deleteBtn.dataset.id)
+        controls.appendChild(readBtn);
+        controls.appendChild(deleteBtn);
+        bookentry.appendChild(controls);
+        bookentry.addEventListener('mouseover', (e)=>{
+            const deleteBtns = document.querySelectorAll('.deleteBtn');
+            deleteBtns.forEach(btn =>{
+            // console.log(deleteBtns)
+            btn.addEventListener('click', deleteBook(btn))
+})
+            
+                controls.style.display = "block";
+            })
+            bookentry.addEventListener('mouseout', (e)=>{
+                controls.style.display = "none";
+            })
+        }
+        )
+
+    }
+
+function deleteBook(button){
+    const id = button.dataset.id;
+    myLibrary.splice(id, 1)
+    console.log(id)
+    console.log(myLibrary)
+};
+
+
